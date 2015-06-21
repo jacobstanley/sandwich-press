31431e4cd5b0f4dc7ca4f77d37ab90aa7d1e4a7a
Jacob Stanley <jacob@stanley.io>
2015-06-21 18:00:09 +1000

    Playing with abstracting over repos

--- src/Main.hs
+++ src/Main.hs
@@ -1,10 +1,13 @@
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Main where
 
 import Control.Monad.IO.Class (liftIO)
 import Data.Monoid ((<>))
+import GHC.TypeLits (Symbol)
 
 import Network.Docker
 
@@ -12,26 +15,40 @@ import Network.Docker
 
 main :: IO ()
 main = runDocker $ do
-    id0 <- build (dnsMasq    :: Dockerfile "centos" "6")
-    id1 <- build (oracleJDK7 :: Dockerfile "centos" "6")
-    id2 <- build (oracleJDK7 :: Dockerfile "centos" "7")
-    liftIO (mapM_ print [id0, id1, id2])
-    xs <- getImages
-    liftIO (mapM_ print xs)
+    --id0 <- build (dnsMasq    :: Dockerfile "centos" "6")
+    --id1 <- build (oracleJDK7 :: Dockerfile "centos" "6")
+    --id2 <- build (oracleJDK7 :: Dockerfile "centos" "7")
+    id3 <- build (oracleJDK7 :: Dockerfile "fedora" "21")
+    id4 <- build (oracleJDK7 :: Dockerfile "fedora" "22")
+    --liftIO (mapM_ print [id0, id1, id2, id3, id4])
+
+    return ()
 
 ------------------------------------------------------------------------
 
-dnsMasq :: Dockerfile "centos" tag
+class Yum (repo :: Symbol)
+
+instance Yum "centos"
+instance Yum "fedora"
+
+class Apt (repo :: Symbol)
+
+instance Apt "ubuntu"
+instance Apt "debian"
+
+------------------------------------------------------------------------
+
+dnsMasq :: Yum repo => Dockerfile repo tag
 dnsMasq = run ["yum", "install", "-y", "dnsmasq"]
 
 ------------------------------------------------------------------------
 
-wget :: Dockerfile "centos" tag
+wget :: Yum repo => Dockerfile repo tag
 wget = run ["yum", "install", "-y", "wget"]
 
 ------------------------------------------------------------------------
 
-oracleJDK7 :: Dockerfile "centos" tag
+oracleJDK7 :: Yum repo => Dockerfile repo tag
 oracleJDK7 = wget <> runMany [fetch, install, rm]
   where
     fetch = [ "wget", "--no-cookies"
